153ee3374f701c2c0f2f2a2680a4497c9e7f03f4
yuuki sakai <s.yuuki0206@gmail.com>
2023-01-07 10:59:24 +0900

    init riverpod

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:river_raddit/firebase_options.dart';
+import 'package:river_raddit/src/feature/auth/screen/login_screen.dart';
+import 'package:river_raddit/src/theme/pallete.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: Pallete.darkModeAppTheme,
+        home: const LoginScreen());
+  }
+}
