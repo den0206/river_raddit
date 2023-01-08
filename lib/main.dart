3e5305d3e388835d202c007174c3966f94959791
yuuki sakai <s.yuuki0206@gmail.com>
2023-01-08 13:45:17 +0900

    auth state change

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:river_raddit/firebase_options.dart';
-import 'package:river_raddit/src/feature/auth/screen/login_screen.dart';
+import 'package:river_raddit/src/core/common/error_text.dart';
+import 'package:river_raddit/src/core/common/loader.dart';
+import 'package:river_raddit/src/core/constants/router.dart';
+import 'package:river_raddit/src/feature/auth/controller/auth_controller.dart';
+import 'package:river_raddit/src/model/user.dart';
 import 'package:river_raddit/src/theme/pallete.dart';
+import 'package:routemaster/routemaster.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,14 +20,53 @@ void main() async {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
   @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  UserModel? userModel;
+
+  Future<void> getUserData(WidgetRef ref, User data) async {
+    userModel = await ref
+        .watch(authControllerProvider.notifier)
+        .getUserData(data.uid)
+        .first;
+    ref.read(userProvider.notifier).update((state) => userModel);
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: Pallete.darkModeAppTheme,
-        home: const LoginScreen());
+    return ref.watch(authStateChangeProvider).when(
+          data: (data) {
+            return MaterialApp.router(
+              title: 'Flutter Demo',
+              theme: Pallete.darkModeAppTheme,
+              routerDelegate: RoutemasterDelegate(routesBuilder: (context) {
+                if (data != null) {
+                  getUserData(ref, data);
+                  if (userModel != null) {
+                    return loggedInRoute;
+                  }
+                }
+                return loggedOutRoute;
+              }),
+              routeInformationParser: const RoutemasterParser(),
+            );
+          },
+          error: (error, stackTrace) {
+            return ErrorText(error: error.toString());
+          },
+          loading: () => const Loader(),
+        );
   }
 }
